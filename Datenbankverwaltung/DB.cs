b6ddbcc467225f0384c68f6c41a50a85c12cb50a
THEN00P <mincraftersmine@gmail.com>
2020-01-22 15:30:06 +0100

    setup

---
+++ Datenbankverwaltung/DB.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Data.OleDb;
+
+namespace Datenbankverwaltung {
+    public class DB {
+        public OleDbConnection dbConnection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=./ProjektDB.accdb");
+    }
+}
